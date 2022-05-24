fa6baeca95a1d6f3689cab963ff53c9c48e2040a
TuiKhumBikCode <103924147+TuiKhumBikCode@users.noreply.github.com>
2022-05-25 00:48:22 +0700

    MVC edit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const { dirname } = require('path');
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(morgan('combined'))
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.engine('hbs', engine({
   extname: '.hbs'
@@ -16,9 +23,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+//route init
+route(app)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
